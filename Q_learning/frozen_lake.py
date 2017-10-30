58bdae645c78caa38b86ec9182a29cfc259e92dc
Leonardo Araujo dos Santos <leonardoaraujo.santos@gmail.com>
2017-10-30 13:59:17 +0000

    Trying tabular Q learning

--- Q_learning/frozen_lake.py
+++ Q_learning/frozen_lake.py
@@ -1,6 +1,28 @@
 # Add some libraries
 import gym
 import numpy as np
+from random import randint
+
+
+# Some references:
+# https://datascience.stackexchange.com/questions/22994/simple-q-table-learning-understanding-example-code
+def epsilon_greedy(possible_actions, value_table, curr_st, epsilon_prob):
+    # Get the greedy and random action
+    greedy_action = np.argmax(value_table[curr_st, :])
+    random_action = randint(0, possible_actions-1)
+
+    # Check if greedy action is available if not the only doable thing is random
+    if greedy_action != None:
+        dice = np.random.random()
+        if dice < epsilon_prob:
+            # Return random action
+            return random_action
+        else:
+            # Return Greedy action
+            return greedy_action
+    else:
+        # No action to take from the value table so return random action
+        return random_action
 
 # Format numpy array print
 np.set_printoptions(precision=3, suppress=True)
@@ -11,15 +33,15 @@ env.reset()
 env.render()
 
 # Set learning parameters
-lr = .8
+lr = .1
 # Gamma is the discounted future reward parameter
 gamma = .95
 num_episodes = 20000
 
 # Initialize table with all zeros (Rows: states(env.observation_space.n), cols: actions(env.action_space.n)
 Q = np.zeros([env.observation_space.n, env.action_space.n])
-print ("Starting Q-Table Values")
-print(Q)
+target_q = np.zeros([env.observation_space.n])
+print ("Starting Q-Table Values (Initialized with Zeros)")
 
 # Create lists to contain total rewards and steps per episode
 rList = []
@@ -32,17 +54,32 @@ for episode in range(num_episodes):
 
     # Play until the end of the game (one full episode)
     while True:
-        # Choose an action greedily (with noise) picking from Q table (Exploration vs Exploitation)
-        a = np.argmax(Q[curr_state, :] + np.random.randn(1, env.action_space.n) * (1. / (episode + 1)))
+        # Decay to choose random more frequently initially
+        epsilon = (1. / ((episode / 50) + 1)) # 100
+        # Run epsilon-greedy to choose actions
+        a = epsilon_greedy(env.action_space.n, Q, curr_state, epsilon)
 
-        # Do some action, then get reward and states
+        # Do some action, then get reward and next states
         next_state, reward, done, info = env.step(a)
+
+        # Get target
+        if done:
+            target_q[curr_state] = reward
+            next_state = env.reset()
+            done = False
+            print(Q)
+        else:
+            target_q[curr_state] = reward + gamma * np.max(Q[next_state, :])
+
+        Q[curr_state, a] = (1.0 - lr)*Q[curr_state, a] + (lr * target_q[curr_state])
+
         # Bellman equation to update Q-Table (Value iteration update)
-        Q[curr_state, a] += lr * (reward + gamma * np.max(Q[next_state, :]) - Q[curr_state, a])
+        #Q[curr_state, a] += lr * (reward + gamma * np.max(Q[next_state, :]) - Q[curr_state, a])
 
         rewards_episode += reward
         curr_state = next_state
         if done:
+            print(Q)
             break
     # Append sum of all rewards on this game
     rList.append(rewards_episode)
